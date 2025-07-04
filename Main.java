fba2b609ba34edec9b6902a78cea75e6de92003a
ASSistant Chef of the 4th Axis Armenia Batallion <160565653+FlyingOwOL@users.noreply.github.com>
2025-07-04 15:34:09 +0800

    Finished all Pop ups views

--- Main.java
+++ Main.java
@@ -7,12 +7,15 @@ import Models.Entry.*;
 import Views.*;
 import Views.AddEntryPopUps.AddEvent;
 import Views.AddEntryPopUps.AddJournal;
+import Views.AddEntryPopUps.AddMeeting;
+import Views.AddEntryPopUps.AddTask;
 import Views.Add_Delete_Calendar_PopUps.AddCalendarFrame;
 import Views.Add_Delete_Calendar_PopUps.DeleteCalendarFrame;
 import Views.Add_Delete_Calendar_PopUps.ViewJournal;
 
 public class Main {
     public static void main(String[] args){
-        new AddJournal();
+        new AccountLoginPage();
+        new AccountPage();
     }    
 }
