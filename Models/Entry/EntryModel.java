474c91ad6467950f4ac20864a9a3dd9ebbfdec6a
ChrisanderJerv <chrisander_yu@dlsu.edu.ph>
2025-07-12 15:07:05 +0800

    .

--- Models/Entry/EntryModel.java
+++ Models/Entry/EntryModel.java
@@ -1,42 +1,93 @@
-package Models.Entry;
+package Models.Entry;               // We use this package to organize our code 
 
-import java.time.LocalDate;
+import java.time.LocalDate;         // We use this to get the current date
 
+/**
+ * This class represents a general calendar entry with a title, optional details, and a date.
+ * 
+ * This is the base class for other specific types of entries like Tasks, Events, Meetings, and Journals.
+ * Each entry must have a title and date, while details are optional.
+ * 
+ */
 public class EntryModel {
-    private String title;
-    private String details;
-    private LocalDate date;
+    private String title;           // The title of the entry
+    private String details;         // Optional details about the entry
+    private LocalDate date;         // The date of the entry
 
+    /**
+     * This constructor constructs a new EntryModel with the specified title.
+     * It initializes details as an empty string and the date as the current date.
+     *
+     * @param title the title of the entry
+     */
     public EntryModel(String title){
         this.title = title;
         this.details = "";
         this.date = LocalDate.now();
     }
 
-    //getters
+    // Getters
+
+    /**
+     *  This getter gets the title of the entry.
+     * 
+     * @return the title of the entry
+     * 
+     */
     public String getTitle() {
         return title;
     }
+
+    /**
+     * This getter gets the details of the entry.
+     * 
+     * @return the details of the entry
+     */
     public String getDetails() {
         return details;
     }
+
+    /**
+     * This getter gets the date of the entry.
+     * 
+     * @return the date of the entry
+     */
     public LocalDate getDate() {
         return date;
     }
 
-    //setters
+    // Setters
+
+    /**
+     * This setter sets the title of the entry.
+     * 
+     * @param title the new title of the entry
+     */
     public void setTitle(String title) {
         this.title = title;
     }
+
+    /**
+     * This setter sets the details of the entry.
+     * 
+     * @param details the new details of the entry
+     */
     public void setDetails(String details) {
         this.details = details;
     }
+
+    /**
+     * This setter sets the date of the entry.
+     * 
+     * @param date the new date of the entry
+     */
     public void setDate(LocalDate date) {
         this.date = date;
     }
 
 
     //@Override
+    // This method returns a string representation of the entry.
     public String toString() {
         return "Title: " + title + "\nDetails: " + details + "\nDate: " + date;
     }
--- Models/Entry/Event.java
+++ Models/Entry/Event.java
@@ -1,44 +1,88 @@
-package Models.Entry;
+package Models.Entry;                       // We use this package to organize our code  
 
-import Models.Account.AccountModel;
+import Models.Account.AccountModel;         // We use this to represent the owner of the event
+import java.time.LocalDate;                 // We use this to get the start and end dates of the event 
 
-import java.time.LocalDate;
-
+/**
+ * This class represents an event entry in the calendar.
+ * 
+ * An event has a title, venue, organizer, and start and end dates.
+ * It extends the EntryModel class to inherit a common title.
+ * 
+ */
 public class Event extends EntryModel{
-    private String venue;
-    private String organizer;
-    private LocalDate startDate;
-    private LocalDate endDate;
+    private String venue;           // The venue of the event
+    private String organizer;       // The organizer of the event
+    private LocalDate startDate;    // The start date of the event
+    private LocalDate endDate;      // The end date of the event
     
+    /**
+     * This constructor constructs a new Event with the specified venue, and owner.
+     * It initializes the title using the superclass constructor. 
+     * 
+     * @param title  the title of the event
+     * @param venue  the venue of the event
+     * @param owner  the account that created the event (used as organizer)
+     */
     public Event(String title, String venue, AccountModel owner){
         super(title);
+        this.venue = venue;
         this.organizer = owner.getName();
     }
 
-    //getters
+    // Getters
+
+    /**
+     * This getter gets the venue of the event.
+     * 
+     * @return the venue of the event
+     */
     public String getVenue() {
         return venue;
     }
+
+    /**
+     * This getter gets the organizer of the event.
+     * 
+     * @return the organizer of the event
+     */
     public String getOrganizer() {
         return organizer;
     }
 
-    //setters
+    // Setters
+
+    /**
+     * This setter sets the venue of the event.
+     * 
+     * @param venue the new venue of the event
+     */
     public void setStartDate(LocalDate startDate) {
         this.startDate = startDate;
     }
+
+    /**
+     * This setter sets the organizer of the event.
+     * 
+     * @param organizer the new organizer of the event
+     */
     public void setEndDate(LocalDate endDate) {
         this.endDate = endDate;
     }
 
-    /*
+    /**
+     * This setter sets the venue of the event.
      * 
+     * @param venue the new venue of the event
      */
     public boolean isValidStartDate(){
         return true;
     }
-    /*
+    
+    /**
+     * This method checks if the end date is valid.
      * 
+     * @return true if the end date is valid, false otherwise
      */
     public boolean isValidEndDate(){
         return true;
